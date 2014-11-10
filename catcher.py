e0676cde609e4b50599a5dea93d9104c30a7ba4b
Spencer Schack <scs1818@gmail.com>
2014-11-10 15:44:15 -0800

    more work on the tracker

--- catcher.py
+++ catcher.py
@@ -1,24 +1,35 @@
 #! /usr/bin/env python
 
 import numpy as np
-from geometry_msgs.msg import PointStamped
+from scipy.optimize import curve_fit
 import freenect
-import cv
+import cv2
 
 class Find:
 
 	def __init__(self):
-		cv.NamedWindow('Video')
+		self.samples = []
+
+		cv2.namedWindow('controls', cv2.CV_WINDOW_AUTOSIZE)
+		def nothing(one): pass
+		cv2.createTrackbar('range_min', 'controls', 0, 255, nothing)
+		cv2.createTrackbar('range_max', 'controls', 255, 255, nothing)
 		while True:
-			self.process_image(freenect.sync_get_depth()[0])
+			depth, timestamp = freenect.sync_get_depth()
+			self.process_image(depth)
 
 	def process_image(self, depth):
-		np.clip(depth, 0, 2**10 - 1, depth)
+		depth >>= 2
 		depth = depth.astype(np.uint8)
-		image = cv.CreateImageHeader((depth.shape[1], depth.shape[0]), cv.IPL_DEPTH_8U, 1)
-		cv.SetData(image, depth.tostring(), depth.dtype.itemsize * depth.shape[1])
-		cv.ShowImage('Video', image)
-		cv.WaitKey(10)
+		range_min = cv2.getTrackbarPos('range_min', 'controls')
+		range_max = cv2.getTrackbarPos('range_max', 'controls')
+		obj = cv2.inRange(depth, range_min, range_max)
+		cv2.imshow('depth', depth)
+		cv2.imshow('obj', obj)
+		cv2.waitKey(10)
+
+	def adjust_trajectory(self, sample):
+		pass
 
 if __name__ == '__main__':
 	Find()
